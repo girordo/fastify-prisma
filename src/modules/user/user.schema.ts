b4e01fce18f5940b27bb10050b21b86738e364e8
girordo <tsgiroldo@gmail.com>
2022-05-02 19:50:35 -0300

    :card_file_box: | Configuring UserInput schema

---
+++ src/modules/user/user.schema.ts
@@ -0,0 +1,21 @@
+import { z } from "zod";
+
+const createUserSchema = z.object({
+  email: z
+    .string({
+      required_error: "Email is required",
+      invalid_type_error: "Email must be a string",
+    })
+    .email()
+    .max(255)
+    .min(3),
+  name: z.string(),
+  password: z
+    .string({
+      required_error: "Password is required",
+      invalid_type_error: "Password must be a string",
+    })
+    .min(8),
+});
+
+export type CreateUserInput = z.infer<typeof createUserSchema>;
