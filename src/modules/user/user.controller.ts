bab2299bd94f12cfee40f7198efe12856ec45a28
girordo <tsgiroldo@gmail.com>
2022-06-03 15:04:54 -0300

    :art: | Changing user controller

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,24 +1,24 @@
-import { FastifyReply, FastifyRequest } from "fastify";
-import { server } from "../../app";
-import { verifyPassword } from "../../utils/hash";
-import { CreateUserInput, LoginInput } from "./user.schema";
-import { createUser, findUserByEmail, findUsers } from "./user.service";
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { server } from '../../app'
+import { verifyPassword } from '../../utils/hash'
+import { CreateUserInput, LoginInput } from './user.schema'
+import { createUser, findUserByEmail, findUsers } from './user.service'
 
 export async function registerUserHandler(
   request: FastifyRequest<{
-    Body: CreateUserInput;
+    Body: CreateUserInput
   }>,
   reply: FastifyReply
 ) {
-  const body = request.body;
+  const body = request.body
 
   try {
-    const user = await createUser(body);
+    const user = await createUser(body)
 
-    return reply.code(201).send(user);
+    return await reply.code(201).send(user)
   } catch (e) {
-    console.log(e);
-    return reply.code(500);
+    console.log(e)
+    return await reply.code(500)
   }
 }
 
@@ -26,31 +26,31 @@ export async function loginHandler(
   request: FastifyRequest<{ Body: LoginInput }>,
   reply: FastifyReply
 ) {
-  const body = request.body;
+  const body = request.body
 
-  const user = await findUserByEmail(body.email);
+  const user = await findUserByEmail(body.email)
 
   if (!user) {
-    return reply.code(401).send({
-      message: "Invalid email or password",
-    });
+    return await reply.code(401).send({
+      message: 'Invalid email or password'
+    })
   }
 
   const correctPassword = verifyPassword({
     candidatePassword: body.password,
     salt: user.salt,
-    hash: user.password,
-  });
+    hash: user.password
+  })
 
   if (correctPassword) {
-    const { password, salt, ...rest } = user;
+    const { password, salt, ...rest } = user
 
-    return { accessToken: server.jwt.sign(rest) };
+    return { accessToken: server.jwt.sign(rest) }
   }
 }
 
 export async function getUsersHandler() {
-  const users = await findUsers();
+  const users = await findUsers()
 
-  return users;
+  return users
 }
